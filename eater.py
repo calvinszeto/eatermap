8456a7ba91308c8551d795c6a835e4b9656e9504
Calvin Szeto <szeto.calvin@gmail.com>
2014-06-10 17:54:25 -0500

    Remove absolute link to data.

--- eater.py
+++ eater.py
@@ -25,7 +25,7 @@ def locate_nearest():
     address = request.args.get('address', 'Austin, TX', type=str)
     page = request.args.get('page', 0, type=int)
     per_page = request.args.get('per_page', 10, type=int)
-    locator = Locator("/home/puppyplus/Projects/eater/data/geocoded_restaurants.json") 
+    locator = Locator("./data/geocoded_restaurants.json") 
     curr_location, nearest_locations = locator.nearest(address, page, per_page)
     return jsonify({"loc": curr_location, "results":[location[0] for location in nearest_locations]})
 
